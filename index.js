c1f2bdba60511c43ad6595b5f62985a58a52c7f0
최민준 <choeminjun@choeminjun-ui-MacBookPro.local>
2020-05-27 23:22:52 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://minjun:890720@boilerplate-b5dog.mongodb.net/test?retryWrites=true&w=majority',{
+   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
+}).then(() => console.log('MongoDb Connected...'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!~~ 안녕하세요'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
